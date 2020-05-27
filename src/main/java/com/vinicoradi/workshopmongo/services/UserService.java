d88dc08d5952b6448b28a9b395d61fdaec698bfe
vini-coradi <vinicesar.coradi@gmail.com>
2020-05-27 19:51:49 -0300

    Inserção de usuário com POST

--- src/main/java/com/vinicoradi/workshopmongo/services/UserService.java
+++ src/main/java/com/vinicoradi/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vinicoradi.workshopmongo.domain.User;
+import com.vinicoradi.workshopmongo.dto.UserDTO;
 import com.vinicoradi.workshopmongo.repository.UserRepository;
 import com.vinicoradi.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
