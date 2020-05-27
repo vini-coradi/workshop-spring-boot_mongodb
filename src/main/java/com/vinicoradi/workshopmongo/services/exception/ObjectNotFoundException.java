bfc989cf9381ec247977ad36d72fc3ade762f4e5
vini-coradi <vinicesar.coradi@gmail.com>
2020-05-27 19:37:03 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/vinicoradi/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.vinicoradi.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);	
+	}
+
+}
