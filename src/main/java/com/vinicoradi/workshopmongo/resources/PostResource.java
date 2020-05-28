9cfc4d6168176edc265b8fbe9d0d26e37d1c2190
vini-coradi <vinicesar.coradi@gmail.com>
2020-05-28 12:00:25 -0300

    Consulta simples com query method

--- src/main/java/com/vinicoradi/workshopmongo/resources/PostResource.java
+++ src/main/java/com/vinicoradi/workshopmongo/resources/PostResource.java
@@ -1,21 +1,17 @@
 package com.vinicoradi.workshopmongo.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vinicoradi.workshopmongo.domain.Post;
-import com.vinicoradi.workshopmongo.domain.User;
-import com.vinicoradi.workshopmongo.dto.UserDTO;
+import com.vinicoradi.workshopmongo.resources.util.URL;
 import com.vinicoradi.workshopmongo.services.PostService;
 
 @RestController
@@ -33,6 +29,14 @@ public class PostResource {
 		
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+	@RequestMapping(value= "/titlesearch", method = RequestMethod.GET)  // or just @GetMapping
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value= "text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 		
 	}
 	
--- src/main/java/com/vinicoradi/workshopmongo/services/PostService.java
+++ src/main/java/com/vinicoradi/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.vinicoradi.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
