2e414104820e2cdc6b390bb5f655d957648379bf
vini-coradi <vinicesar.coradi@gmail.com>
2020-05-28 10:49:18 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/vinicoradi/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.vinicoradi.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vinicoradi.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
