6eceb6cfed1318d5d3833e4e288aaa7139e2473c
vini-coradi <vinicesar.coradi@gmail.com>
2020-05-28 11:05:53 -0300

    Referenciando os posts do usuário

--- src/main/java/com/vinicoradi/workshopmongo/domain/User.java
+++ src/main/java/com/vinicoradi/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.vinicoradi.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document    // é possivel colocar o nome usando (collection="user")
@@ -15,6 +18,10 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	
 	public User() {
 		
@@ -49,6 +56,14 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
