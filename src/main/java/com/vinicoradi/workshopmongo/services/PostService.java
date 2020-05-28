e07257bc73c3aaf04e00e5754f2bda52856ec3bb
vini-coradi <vinicesar.coradi@gmail.com>
2020-05-28 11:16:19 -0300

    Obtendo um post por id

---
+++ src/main/java/com/vinicoradi/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.vinicoradi.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinicoradi.workshopmongo.domain.Post;
+import com.vinicoradi.workshopmongo.repository.PostRepository;
+import com.vinicoradi.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+}
